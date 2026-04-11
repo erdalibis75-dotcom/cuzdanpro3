5eedb34b1de78027bb2d9c63c11e255d567e3877
erdalibis75-dotcom <erdalibis75@gmail.com>
2026-04-11 20:57:31 +0300

    Update Service-worker.js

--- Service-worker.js
+++ Service-worker.js
@@ -1,32 +1,18 @@
-const CACHE_NAME = 'cuzdan-pro-v1';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json'
+const CACHE_NAME = 'cuzdan-v1';
+const ASSETS = [
+  '/cuzdanpro3/',
+  '/cuzdanpro3/index.html',
+  'https://googleapis.com'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
-  self.clients.claim();
 });
